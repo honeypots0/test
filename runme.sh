b9a7fcdd279d247e17ea2a6fd3f024f9d0f13e81
honeypots0 <76939125+honeypots0@users.noreply.github.com>
2021-03-26 13:44:18 +0530

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,5 +1,6 @@
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install selenium
-sudo apt install tor proxychains
+pip3 install pyautogui
+sudo apt install tor proxychains -y
 cp geckodriver /usr/local/bin
 chmod 777 /usr/local/bin/geckodriver
